11612ad07d4176cb6c868d145534538f2dde0a3d
jhyhy0528 <hjeongg23@gmail.com>
2024-10-18 16:31:39 -0500

    Delete CompareCriteria.java

--- CompareCriteria.java
+++
@@ -1,9 +0,0 @@
-/**
- * Enum for the three different criteria that could be chosen to determine how to 
- * compare Tasks and their organization in the TaskQueue.
- * @author Michelle
- *
- */
-public enum CompareCriteria {
-  TITLE, LEVEL, TIME
-}
