7d689b3f7c125530f897483e848f81f94dbe6c72
jhyhy0528 <hjeongg23@gmail.com>
2024-10-18 16:34:48 -0500

    Delete Movable.java

--- Movable.java
+++
@@ -1,9 +0,0 @@
-public interface Movable {
-
-    /**
-     * Defines how this movable object moves.
-     * This method is intended to be implemented to provide specific moving behaviors.
-     */
-    void move();
-    
-}
